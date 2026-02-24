e780a130ff0d0a7b7aae29ad52956488477a3065
Yechan Moon <moonyechan97@naver.com>
2026-02-24 20:34:40 +0900

    week1 A 2309 B1

--- src/baekjoon/week/week1/a2309_b1/Standard.java
+++ src/baekjoon/week/week1/a2309_b1/Standard.java
@@ -1,8 +1,46 @@
 package baekjoon.week.week1.a2309_b1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class Standard {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int[] a = new int[9];
+        int sum = 0;
+
+        for (int i = 0; i < 9; ++i) {
+            a[i] = Integer.parseInt(br.readLine());
+            sum += a[i];
+        }
+
+        int x = -1, y = -1;
+
+        for (int i = 0; i < 9; ++i) {
+            for (int j = i + 1; j < 9; ++j) {
+                if (sum - a[i] - a[j] == 100) {
+                    x = i;
+                    y = j;
+                }
+            }
+            if (x != -1) break;
+        }
+
+        int[] ans = new int[7];
+        int idx = 0;
+        for (int i = 0; i < 9; ++i) {
+            if (i == x || i == y) continue;
+            ans[idx++] = a[i];
+        }
+
+        Arrays.sort(ans);
+
+        StringBuilder sb = new StringBuilder();
+        for (int n : ans) sb.append(n).append('\n');
+        System.out.println(sb.toString());
     }
 }
--- src/baekjoon/week/week1/a2309_b1/TwoPointer.java
+++ src/baekjoon/week/week1/a2309_b1/TwoPointer.java
@@ -1,8 +1,38 @@
 package baekjoon.week.week1.a2309_b1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class TwoPointer {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int[] a= new int[9];
+        int sum = 0;
+        for (int i = 0; i < 9; ++i) {
+            a[i] = Integer.parseInt(br.readLine());
+            sum += a[i];
+        }
+
+        Arrays.sort(a);
+
+        int l = 0;
+        int r = 8;
+
+        while (l < r) {
+            if (a[l] + a[r] < sum - 100) ++l;
+            else if (a[l] + a[r] > sum - 100) --r;
+            else break;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 9; ++i) {
+            if (i == l || i == r) continue;
+            sb.append(a[i]).append('\n');
+        }
+        System.out.println(sb);
     }
 }
